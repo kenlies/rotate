b23191cbfec89f748bd03cee99be4c16adaca1e6
alexander <alexander.otsala@gmail.com>
2025-02-05 18:04:30 +0200

    Game.hpp, constants.h: Add constants.h file for constant values

--- includes/Game.hpp
+++ includes/Game.hpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <memory>
 #include <random>
+#include "constants.h"
 #include "ResourceManager.hpp"
 #include "Player.hpp"
 #include "Box.hpp"
@@ -14,24 +15,6 @@
 #include "BoxMap.hpp"
 #include "BoxParticles.hpp"
 #include "Candle/RadialLight.hpp"
-
-#define GRAVITY_MAGNITUDE 18.f
-#define JUMP_FORCE 20.f
-#define SCALE 30.f
-#define BOX_WIDTH 32.f
-#define CIRCLE_RADIUS 16.f
-#define ROTATE_SPEED 2.f
-#define EXPLOSION_FORCE 15.f
-#define WIN_FORCE 10.f
-#define PICKUP_FORCE 1.f
-
-// identifiers
-#define RED 1
-#define WHITE 2
-#define CYAN 3
-#define GREEN 4
-#define YELLOW 5
-#define PLAYER 6
 
 class Player;
 class Hud;
---
+++ includes/constants.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define GRAVITY_MAGNITUDE 18.f
+#define JUMP_FORCE 20.f
+#define SCALE 30.f
+#define BOX_WIDTH 32.f
+#define CIRCLE_RADIUS 16.f
+#define ROTATE_SPEED 2.f
+#define EXPLOSION_FORCE 15.f
+#define WIN_FORCE 10.f
+#define PICKUP_FORCE 1.f
+
+// identifiers
+#define RED 1
+#define WHITE 2
+#define CYAN 3
+#define GREEN 4
+#define YELLOW 5
+#define PLAYER 6
