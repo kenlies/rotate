93dba8e89096fe6a9bdb07b6676e2d96c8523375
alexander <alexander.otsala@gmail.com>
2025-02-13 20:10:42 +0200

    Hud.hpp: Organize stuff

--- includes/Hud.hpp
+++ includes/Hud.hpp
@@ -10,14 +10,15 @@ class Hud : public sf::Drawable, public sf::Transformable {
 		Hud(Game *game);
 		~Hud();
 
-		void updateScore(unsigned short score);
-		virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+		void			updateScore(unsigned short score);
+		virtual void	draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
 	private:
-		Game *_game;
-		sf::Font _scoreFont;
-		sf::Text _scoreText;
-		sf::Text _scoreAvailableText;
-		sf::Text _scoreSlash;
+		Game		*_game;
+		sf::Font	_scoreFont;
+		sf::Text	_scoreText;
+		sf::Text	_scoreAvailableText;
+		sf::Text	_scoreSlash;
 
 		std::unique_ptr<candle::RadialLight> _scoreLight;
 };
