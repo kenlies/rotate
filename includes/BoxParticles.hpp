1f81009a84fef06623f0c180f94701b0fd8a2397
alexander <alexander.otsala@gmail.com>
2025-02-13 20:08:41 +0200

    BoxParticles.hpp: Organize stuff

--- includes/BoxParticles.hpp
+++ includes/BoxParticles.hpp
@@ -9,24 +9,22 @@ class BoxParticles : public sf::Drawable, public sf::Transformable {
 		BoxParticles(unsigned int count, sf::Vector2f position);
 		~BoxParticles();
 
-		void update(sf::Time elapsed);
-		virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+		void 			update(sf::Time elapsed);
+		virtual void	draw(sf::RenderTarget& target, sf::RenderStates states) const;
 
 		// ---- getters ----
-
 		const sf::Time &getCurrLife() const;
 		
 	private:
-
 		struct Particle
 		{
-			sf::Vector2f velocity;
-			sf::Time lifetime;
+			sf::Vector2f	velocity;
+			sf::Time		lifetime;
 		};
 
-		std::vector<Particle> m_particles;
-		sf::VertexArray m_vertices;
-		sf::Time m_lifetime;
-		sf::Time m_currlife;
-		sf::Vector2f m_emitter;
+		std::vector<Particle>	m_particles;
+		sf::VertexArray 		m_vertices;
+		sf::Time				m_lifetime;
+		sf::Time				m_currlife;
+		sf::Vector2f			m_emitter;
 };
