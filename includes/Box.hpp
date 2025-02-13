1adbe0731b0dfe61210331bddb482e26978a7055
alexander <alexander.otsala@gmail.com>
2025-02-13 20:06:36 +0200

    Box.hpp: Organize stuff

--- includes/Box.hpp
+++ includes/Box.hpp
@@ -13,13 +13,13 @@ class Box {
 		bool isInView(const sf::View &view) const;
 
 		// ---- getters ----
-		b2Body*				getBody() const;
+		b2Body*										getBody() const;
 		const std::unique_ptr<sf::RectangleShape>	&getShape() const;
-		const std::unique_ptr<candle::RadialLight> &getLight() const;
+		const std::unique_ptr<candle::RadialLight>	&getLight() const;
 		
 	private:
-		Game*				_game;
-		b2Body* 			_body;
-		std::unique_ptr<sf::RectangleShape>	_shape;
-		std::unique_ptr<candle::RadialLight> _light;
+		Game*									_game;
+		b2Body* 								_body;
+		std::unique_ptr<sf::RectangleShape>		_shape;
+		std::unique_ptr<candle::RadialLight>	_light;
 };
